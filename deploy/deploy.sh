4f5a8afc149e118a910c5f6e750e3e20a69cccbd
Tom Whitwell <tom@whitwell.xyz>
2018-04-09 22:31:20 +0100

    update to use secret

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,10 +2,12 @@
 REG=${1}
 TAG=${2}
 NS=${3}
+GL_TOKEN=${4}
 URL=${4}
 export REGISTRY=${REG}
 export BUILD_NUMBER=${TAG}
 export NAMESPACE=${NS}
+export GITLAB_TOKEN=${GL_TOKEN}
 export ENV_URL=${URL}
 for f in deploy/k8s/*.yml
 do

f8a131f58df9b9c7f48c70b74d8d90b54f4d20cc
Tom Whitwell <tom@whitwell.xyz>
2018-04-09 22:00:40 +0100

    oops

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ export REGISTRY=${REG}
 export BUILD_NUMBER=${TAG}
 export NAMESPACE=${NS}
 export ENV_URL=${URL}
-for f in templates/*.yml
+for f in deploy/k8s/*.yml
 do
  envsubst < $f > “.generated/$(basename $f)”
 done
